0c55794a7747f4d63ad3c432b705ffd1966eae85
galvanizejs <js@matthewleach.com>
2016-08-22 14:17:53 -0600

    Initial Post

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# prep-week4-project1
+# js-week4-project1
+
+This project will give you an opprotunity to start practicing with jQuery. 
---
+++ main.js
@@ -0,0 +1,86 @@
+/*
+=======================================================
+
+ ** Week 4 - Project 1  **
+*** Exercise the jQuery ***
+
+We'll be using the html code within the index.html for the 
+following jQuery exercises. After you've completed an exercise, 
+comment out your code (unless otherwise indicated) as these 
+techniques may conflict with each other.
+ 
+=======================================================
+*/
+
+// Add your JS here.
+
+
+
+
+// 1. Make sure that the HTML document is fully loaded before running any jQuery code that is needed below. Make sure you don't comment out this code as you'll wrap this code around all the following methods (so don't forget the closing tags, too. 
+
+    
+   
+    
+// 2. Hide all of the paragraphs on the index.html page.
+
+
+    
+    
+    
+// 3. Hide all of the h2's on the page.
+
+
+    
+
+
+// 4. Hide all images on the page.
+  
+	
+	
+	
+	
+// 5. Hide all links on the page.
+
+    
+	
+	
+	
+// 6. Hide all of the elements in the document.
+
+    
+    
+ 
+ 
+// 7. Hide all paragraphs when one of them is clicked.
+
+
+
+
+    
+// 8. Hide all images when one of them is clicked.
+
+
+
+
+
+// 9. When the user clicks onto the image, toggle the paragraphs between hide and show.  
+
+ 
+ 
+ 
+ 
+/*
+SANDBOX: 
+Based on the code that is used to hide all images when clicked, consider testing the following options to test the interaction. 
+
+Interactive Methods: dblclick(), hover(), mousedown(), mouseenter(), mouseleave(), mousemove(), mouseout(), mouseover(), and mouseup().
+       
+	 
+Update previous hide functions with speed. 
+
+How does the speed affect the animated techniques as they are applied to different HTML elements?  
+
+*/
+
+
